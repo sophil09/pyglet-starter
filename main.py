30e16298324594e4cc0b7e2e4fc2e28ee71fcd09
me <60446068+sophil09@users.noreply.github.com>
2020-02-28 13:21:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import pyglet # import the library
-win = pyglet.window.Window() # create the window
+import pyglet
 
-# Load the image
-img = pyglet.image.load('assets/hero/Old hero.png')
+win= pyglet.window.Window()
 
-# spr = pyglet.sprite.Sprite(img, x=50, y=50)
+# Create a sprite so the game can keep track of coordinates
+img= pyglet.image.load('assets/hero/sliced/idle-1.png')
+spr = pyglet.sprite.Sprite(img, x = 200, y = 200)
 
-# Start the event loop
+# Get the key state handler object
+keys = pyglet.window.key.KeyStateHandler()
+
 def update(dt):
-  pass
+  win.push_handlers(keys) # update the keys object
+  if keys[pyglet.window.key.LEFT]: # if the key is pressed...
+    spr.x -= 3 # change spr.x by -1
+  if keys[pyglet.window.key.RIGHT]:
+    spr.x += 3
+  if keys[pyglet.window.key.UP]:
+    spr.y += 3
+  if keys[pyglet.window.key.DOWN]:
+    spr.y -= 3
 
 @win.event
 def on_draw():
-    win.clear()
-    img.blit(200, 100)
-    # spr.draw()
+  win.clear()
+  spr.draw()
 
-pyglet.clock.schedule(update) 
+pyglet.clock.schedule(update)
 pyglet.app.run()
